060c09f6000d10f899cb00a5c1f161454bacb7c5
tanisha2702 <agrawaltanishaa@gmail.com>
2026-03-27 14:21:20 +0530

    Update dust_sensor_reader.v

--- src/dust_sensor_reader.v
+++ src/dust_sensor_reader.v
@@ -1,26 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.03.2026 19:26:03
-// Design Name: 
-// Module Name: dust_sensor_reader
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-`timescale 1ns / 1ps
 
 module dust_sensor_reader
 #(
@@ -76,8 +54,5 @@ module dust_sensor_reader
             end
         end
     end
-
-endmodule
-assign dust_val = low_cnt_reg;
 
 endmodule
